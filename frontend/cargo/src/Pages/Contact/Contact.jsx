b7b499da2832646602711e114b536adb79b4f58f
blaise <ojwangblaise145@gmail.com>
2025-12-13 20:44:27 +0300

    4th commit

--- frontend/cargo/src/Pages/Contact/Contact.jsx
+++ frontend/cargo/src/Pages/Contact/Contact.jsx
@@ -26,20 +26,31 @@ const Contact = () => {
           <div className="info-cards">
             <div className="info-card">
               <FaPhoneAlt className="info-icon" />
-              <h3>Phone</h3>
-              <p>+254 700 123 456</p>
+              <div className="info-text">
+                <h3>Phone</h3>
+                <p>+447401149968</p>
+              </div>
             </div>
+
             <div className="info-card">
               <FaEnvelope className="info-icon" />
-              <h3>Email</h3>
-              <p>support@cargocharter.com</p>
+              <div className="info-text">
+                <h3>Email</h3>
+                <p>info@airrushcharters.com</p>
+              </div>
             </div>
+
             <div className="info-card">
               <FaMapMarkerAlt className="info-icon" />
-              <h3>Location</h3>
-              <p>Nairobi, Kenya</p>
+              <div className="info-text">
+                <h3>Location</h3>
+                <p>South Africa</p>
+                <h3>Branch</h3>
+                <p>Nairobi, KENYA</p>
+              </div>
             </div>
           </div>
+
         </div>
 
         <div className="contact-form">
@@ -59,7 +70,7 @@ const Contact = () => {
       <section className="map-section">
         <iframe
           title="Company Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7419693016086!2d36.92623923714047!3d-1.3309072835512266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f129102505cef%3A0xefc58e7ef0660bf2!2sJomo%20Kenyatta%20International%20Airport!5e0!3m2!1sen!2ske!4v1756989577868!5m2!1sen!2ske" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7419693016086!2d36.92623923714047!3d-1.3309072835512266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f129102505cef%3A0xefc58e7ef0660bf2!2sJomo%20Kenyatta%20International%20Airport!5e0!3m2!1sen!2ske!4v1756989577868!5m2!1sen!2ske"
           allowFullScreen=""
           loading="lazy"
         ></iframe>
